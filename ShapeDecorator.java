15df8e4cba7b9c2be7724b20b778fe35b2d5521c
Matthew Cole <120291063+matthewcole12@users.noreply.github.com>
2026-02-19 10:32:31 +0000

    Add ShapeDecorator.java implementing the decorator pattern as an abstract base class

---
+++ ShapeDecorator.java
@@ -0,0 +1,17 @@
+// ShapeDecorator.java
+
+/**
+ * Abstract base class for decorators that implement the decorator pattern.
+ */
+public abstract class ShapeDecorator implements Shape {
+    protected Shape decoratedShape;
+
+    public ShapeDecorator(Shape decoratedShape) {
+        this.decoratedShape = decoratedShape;
+    }
+
+    @Override
+    public void draw() {
+        decoratedShape.draw();
+    }
+}
