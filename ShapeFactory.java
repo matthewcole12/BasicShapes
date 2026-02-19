81a278761e70dc1a4667c753a43697d31e0c453b
Matthew Cole <120291063+matthewcole12@users.noreply.github.com>
2026-02-19 10:24:16 +0000

    Add Triangle support to ShapeFactory

--- ShapeFactory.java
+++ ShapeFactory.java
@@ -1,9 +1,14 @@
 public class ShapeFactory {
-    public Shape getShape(String id) {
-        switch(id.toLowerCase()) {
-            case "square": return new Square();
-            case "circle": return new Circle();
-            default: return null;
+    public static Shape createShape(String type, double... dimensions) {
+        switch(type.toLowerCase()) {
+            case "circle":
+                return new Circle(dimensions[0]);
+            case "square":
+                return new Square(dimensions[0]);
+            case "triangle":
+                return new Triangle(dimensions[0], dimensions[1]);
+            default:
+                throw new IllegalArgumentException("Unknown shape type: " + type);
         }
     }
 }
